873aab9bf6ba90b34f5d6d34e11bd8f52ee1c90c
MonFe <139465420+Nicki-lombardo@users.noreply.github.com>
2024-08-10 14:28:17 +0200

    Add files models

---
+++ models/question.js
@@ -0,0 +1,14 @@
+
+class Question {
+
+    constructor() {
+        this.text = 'question text';
+        this.choices = ['choice 1', 'choice 2', 'choice 3', 'choice 4'];
+        this.answer = 'answer';
+    }
+
+}
+
+console.log('question is connected');
+
+
